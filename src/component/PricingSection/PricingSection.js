7f46a0b589e99555279ced8a5d2090773680295c
Miracle1048 <miraclematthias1@gmail.com>
2024-07-27 12:51:12 +0100

    re

---
+++ src/component/PricingSection/PricingSection.js
@@ -0,0 +1,12 @@
+import "./PricingSection.css"
+function PricingSection(){
+  return(
+    <div className="pricing">
+        <div>
+            
+        </div>
+
+    </div>
+  )  
+}
+export default PricingSection;
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,10 +1,12 @@
 import Header from "../component/Header/Header";
+import PricingSection from "../component/PricingSection/PricingSection";
 import Footer from "../component/FOOTER/footer";
 
 function Pricing(){
  return(
     <div>
   <Header />
+  <PricingSection />
   <Footer />
     </div>
  )
